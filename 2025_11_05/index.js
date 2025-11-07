bd5639796d4f6dc65e3df1398f4d362b32450acf
Wiktor Kycia <wiktor@debian_lepszy.pl>
2025-11-07 19:47:45 +0100

    change mysql to mysql2, use mysql2.pool, use mysql native password in order to authorize the root user

--- 2025_11_05/index.js
+++ 2025_11_05/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
 const mysql_host = 'database'
 const mysql_user = 'root'
@@ -9,13 +9,15 @@ const mysql_database_name = 'database'
 
 const app = express();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: mysql_host,
     user: mysql_user,
     password: mysql_password,
-    database: mysql_database_name
-
-})
+    database: mysql_database_name,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+}).promise();
 
 app.use('/static', express.static(path.join(__dirname, 'static')))
 app.use(express.urlencoded({extended:false}))
@@ -23,30 +25,14 @@ app.use(express.urlencoded({extended:false}))
 const port = 3000;
 
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Błąd połączenia: ' + err.stack)
+(async () => {
+    try {
+        await pool.query('CREATE TABLE IF NOT EXISTS messages (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, firstname VARCHAR(50), lastname VARCHAR(50), email VARCHAR(50), message TEXT)')
+        console.log('Tabela messages gotowa')
+    } catch (err) {
+        console.error('Błąd tworzenia tabeli:', err)
     }
-    else
-    {
-        console.log('Połączono z bazą danych jako ID: ' + connection.threadId)
-    }
-})
-
-connection.query('CREATE TABLE IF NOT EXISTS messages (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, firstname VARCHAR(50), lastname VARCHAR(50), email VARCHAR(50), message TEXT)', (err, results, fields) => {
-    if (err) { throw err }
-    console.log('Wynik zapytania: ' + results)
-})
-
-connection.end((err)=> {
-    if (err) {
-        console.error('Błąd zamknięcia połączenia: ' + err.stack)
-    }
-    else
-    {
-        console.log('Połączenie z bazą danych zostało zamknięte.')
-    }
-})
+})()
 
 
 app.get('/', (req, res) => {
@@ -65,7 +51,7 @@ app.get('/kontakt', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'kontakt.html'));
 })
 
-app.post('/kontakt', (req, res) => {
+app.post('/kontakt', async (req, res) => {
     const firstName = req.body?.first_name ?? '';
     const lastName = req.body?.last_name ?? '';
     const email = req.body?.email ?? ''
@@ -77,26 +63,17 @@ app.post('/kontakt', (req, res) => {
 
     // tutaj trzeba zapisać message do bazy
 
-    connection.connect((err) => {
-        if (err) {
-            console.error('Błąd połączenia: ' + err.stack)
-            res.sendStatus(500)
-        }
-        console.log('Połączono z bazą danych jako ID: ' + connection.threadId)
-    })
-
-    connection.query(`INSERT INTO messages (firstname, lastname, email, message) VALUES (${firstName}, ${lastName}, ${email}, ${message})`, (err, result) => {
-        if (err) throw err
-        console.log('Affected rows: ' + result.affectedRows)
-    })
-
-    connection.end((err) => {
-        if (err) {
-            console.error('Błąd zamknięcia połączenia: ' + err.stack)
-            res.sendStatus(500)
-        }
-        console.log('Połączenie z bazą danych zostało zamknięte.')
-    })
+    try {
+        const [result] = await pool.execute(
+            'INSERT INTO messages (firstname, lastname, email, message) VALUES (?, ?, ?, ?)',
+            [firstName, lastName, email, message]
+        )
+        console.log('Inserted ID: ' + result.insertId)
+        res.redirect(302, '/')
+    } catch (err) {
+        console.error('Insert error:', err)
+        res.sendStatus(500)
+    }
 
 
     res.redirect(302, '/')
