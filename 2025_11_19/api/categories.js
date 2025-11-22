6609a09062b91080dcb3c26a629607a363e45099
wiktor <wiktor.kycia@gmail.com>
2025-11-22 16:45:28 +0100

    add a GET route for categories and test it

--- 2025_11_19/api/categories.js
+++ 2025_11_19/api/categories.js
@@ -9,4 +9,10 @@ dotenvExpand.expand(myenv)
 const router = express.Router()
 const prisma = new PrismaClient()
 
+router.get('/', async (req, res) => {
+    const categories = await prisma.category.findMany()
+    console.log(categories)
+    res.status(200).json(categories)
+})
+
 module.exports = router
