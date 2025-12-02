f32debd3db9040a1e4910cd197c6f0bdb3478a5c
wiktor <wiktor.kycia@gmail.com>
2025-12-02 22:05:42 +0100

    implement mongodb

--- 2025_11_26/index.js
+++ 2025_11_26/index.js
@@ -4,14 +4,19 @@ const categoriesRouter = require('./api/categories')
 const commentsRouter = require('./api/comments')
 const dotenv = require('dotenv')
 const dotenvExpand = require('dotenv-expand')
+const { MongoClient } = require('mongodb')
 
 const myenv = dotenv.config({ path: '.env' })
 dotenvExpand.expand(myenv)
+
+const mongoUrl = process.env.MONGO_URL
 
 const host = process.env.APP_HOST
 const port = process.env.APP_PORT
 
 const app = express()
+
+const mongoClient = new MongoClient(mongoUrl)
 
 function add0(a) {
     if (a < 10) {
@@ -21,7 +26,30 @@ function add0(a) {
     }
 }
 
+const initMongo = async () => {
+    try {
+        const conn = await mongoClient.connect()
+        console.log('Connected to mongo')
+        const dbo = await conn.db("mydb")
+        try {
+            await dbo.createCollection("accessLogs")
+        } catch (e) {
+            console.error(e)
+        }
+        await conn.close()
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+
+
 app.use(express.json())
+
+// app.use(async (req, res, next) => {
+//
+// })
+
 app.use((req, res, next) => {
     console.log('======================================')
     let date = new Date()
@@ -58,6 +86,13 @@ app.all('*', async (req, res) => {
     res.sendStatus(404)
 })
 
-app.listen(port, () => {
-    console.log(`App is running on http://${host}:${port}`)
+;(async () => {
+    await initMongo()
+    app.listen(port, () => {
+        console.log(`App is running on http://${host}:${port}`)
+    })
+})().catch(err => {
+    console.error('Failed to initialize Mongo: ', err)
+    process.exit(1)
 })
+
