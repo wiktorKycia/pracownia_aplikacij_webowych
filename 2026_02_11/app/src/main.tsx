c6919f67e26bb20dddb2297dd984a2e7d327bddf
wiktor <wiktor.kycia@gmail.com>
2026-02-11 09:22:54 +0100

    add query client to main.tsx

--- 2026_02_11/app/src/main.tsx
+++ 2026_02_11/app/src/main.tsx
@@ -3,12 +3,18 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 import './index.scss'
 import App from './App.tsx'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter>
-          <App/>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+              <App/>
+          </BrowserRouter>
+          <ReactQueryDevtools/>
+      </QueryClientProvider>
   </StrictMode>
 )
