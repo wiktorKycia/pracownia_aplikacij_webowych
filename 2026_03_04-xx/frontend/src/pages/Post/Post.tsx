465f10f72790e1cd928074d5b170602efc5c7746
wiktor <wiktor.kycia@gmail.com>
2026-04-02 12:50:13 +0200

    correct the Post page along with the Comment component

--- 2026_03_04-xx/frontend/src/pages/Post/Post.tsx
+++ 2026_03_04-xx/frontend/src/pages/Post/Post.tsx
@@ -3,12 +3,14 @@ import styles from './Post.module.scss'
 import {usePost} from '../../hooks/usePost.ts'
 import Comment from "../../components/Comment/Comment.tsx";
 import type CommentType from "../../types/Comment/Comment.ts";
+import type PostType from "../../types/Post/Post.ts"
 
 export function Post() {
     const id: number = parseInt(useParams().id as string)
     console.log(id)
 
     const {data, isLoading, isError} = usePost(id);
+    const post: PostType | null = data || null;
 
     return (
         <>
@@ -18,22 +20,20 @@ export function Post() {
             {isError && (
                 <div className={styles.PostError}>Wystąpił błąd podczas ładowania wpisu</div>
             )}
-            {!isLoading && !isError && data && (
+            {!isLoading && !isError && (
                 <>
-                    {(data.post == null) && (
+                    {(post == null) && (
                         <div className={styles.PostError}>Brak wpisów</div>
                     )}
-                    {data.post !== undefined && data.post !== null && (
-                        <div className={styles.Post} key={data.post.id}>
-                            <h1 className={styles.PostTitle}>{data.post.title}</h1>
-                            <p className={styles.PostBody}>{data.post.body}</p>
-                            <p className={styles.PostCreator}>Created by&nbsp;
-                                <Link to={`/users/${data.creator.id}`} className={styles.PostCreatorUsername}>{data.creator.username}</Link>
-                            </p>
+                    {post !== undefined && post !== null && (
+                        <div className={styles.Post} key={post.id}>
+                            <h1 className={styles.PostTitle}>{post.title}</h1>
+                            <p className={styles.PostBody}>{post.content}</p>
+
                             <Link to={"/posts/"} className={styles.PostLink}>Wróć do wpisów</Link>
                             <article className={styles.PostCommentContainer}>
-                                {data.comments && (
-                                    data.comments.map((c: CommentType) => <Comment key={c.id} {...c}/>)
+                                {post.comments && (
+                                    post.comments.map((c: CommentType) => <Comment key={c.id} {...c}/>)
                                 )}
                             </article>
                         </div>
