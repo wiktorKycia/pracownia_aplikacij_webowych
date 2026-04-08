6cf7f6b8ae2793b395524315eecb42f75b1df08d
wiktor <wiktor.kycia@gmail.com>
2026-04-08 10:17:58 +0200

    add function to read graph

--- 2026_04_08/main.py
+++ 2026_04_08/main.py
@@ -2,7 +2,15 @@
 filename: str = "graph.txt"
 
 def read_graph(filename: str):
-
+    lines: list[str]
     with open(filename, 'r') as f:
+        lines = f.read().split('\n')
+
+    n: int = int(lines.pop(0))
+    graph: list[list[int]] = []
+    for line in lines:
+        line: list[int] = list(map(int, line.split(' ')))
+        line.pop(0)
+        graph.append(line)
 
     return
